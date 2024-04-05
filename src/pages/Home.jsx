c647e50680aac98246b1b2fff81c6b32ab394eb5
Henna Maria <hennamaria2001@gmail.com>
2024-04-05 11:06:02 +0530

    seo added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import Layout from '../components/layout/Layout'
 
 const Home = () => {
     return (
-        <Layout>
+        <Layout 
+        title={"EcoMart - Your One-Stop Shop for Eco-Friendly Products"}
+        description={"EcoMart offers a wide range of eco-friendly products for a sustainable lifestyle. Shop now for organic, reusable, and environmentally friendly goods. Join us in our mission for a greener future."}
+        keyword={"EcoMart, e-commerce, MERN stack, sustainable products, eco-friendly, organic, reusable, environmentally friendly, green living, sustainable lifestyle, online marketplace."}
+        author={"Henna Maria"}
+        >
             <div>Home</div>
         </Layout>
     )
