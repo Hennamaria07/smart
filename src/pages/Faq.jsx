eea4b8d4e89d108641a23b11b46563ce827f269a
Henna Maria <hennamaria2001@gmail.com>
2024-04-05 11:03:40 +0530

    typo fixed

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/layout/Layout'
 const Faq = () => {
   return (
     <Layout
-    titlt={"EcoMart FAQ - Answers to Your E-commerce Questions"}
+    title={"EcoMart FAQ - Answers to Your E-commerce Questions"}
     description={"Explore EcoMart's comprehensive FAQ section to find answers to commonly asked questions about eco-friendly shopping, sustainability practices, product inquiries, and more."}
     keyword={"EcoMart, FAQ, Frequently Asked Questions, sustainable shopping, eco-friendly products, MERN e-commerce, sustainability practices, eco-conscious consumers, environmental responsibility"}
     author={"Henna Maria"}
