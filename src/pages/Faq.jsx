5ec9aaade29a499d5f7efbdb61c38f8fb58d6dc4
Henna Maria <hennamaria2001@gmail.com>
2024-04-05 11:00:07 +0530

    seo added

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -3,7 +3,12 @@ import Layout from '../components/layout/Layout'
 
 const Faq = () => {
   return (
-    <Layout>
+    <Layout
+    titlt={"EcoMart FAQ - Answers to Your E-commerce Questions"}
+    description={"Explore EcoMart's comprehensive FAQ section to find answers to commonly asked questions about eco-friendly shopping, sustainability practices, product inquiries, and more."}
+    keyword={"EcoMart, FAQ, Frequently Asked Questions, sustainable shopping, eco-friendly products, MERN e-commerce, sustainability practices, eco-conscious consumers, environmental responsibility"}
+    author={"Henna Maria"}
+    >
         <div>Faq</div>
     </Layout>
   )
